3187eb3b40d30a38a4e4a81cd481256df91a55bf
Thomas Tourlourat <thomas@tourlourat.com>
2013-08-24 14:16:47 +0200

    comment geshi

--- src/Wozbe/BlogBundle/Markdown/Parser/GeshiParser.php
+++ src/Wozbe/BlogBundle/Markdown/Parser/GeshiParser.php
@@ -8,16 +8,16 @@ use GeSHi;
 class GeshiParser extends BaseParser
 {
 
-    public function _doFencedCodeBlocks_callback($matches) 
-    {
-        $codeblock = $matches[2];
-        $codeblock = htmlspecialchars($codeblock, ENT_NOQUOTES);
-        $codeblock = preg_replace_callback('/^\n+/',
-                array(&$this, '_doFencedCodeBlocks_newlines'), $codeblock);
-        $geshi = new GeSHi($codeblock, 'php');
-        $code = $geshi->parse_code();
-        
-        $codeblock = '<code>' . $code . '</code>';
-        return $this->hashBlock($codeblock);
-    }
+//    public function _doFencedCodeBlocks_callback($matches) 
+//    {
+//        $codeblock = $matches[2];
+//        $codeblock = htmlspecialchars($codeblock, ENT_NOQUOTES);
+//        $codeblock = preg_replace_callback('/^\n+/',
+//                array(&$this, '_doFencedCodeBlocks_newlines'), $codeblock);
+//        $geshi = new GeSHi($codeblock, 'php');
+//        $code = $geshi->parse_code();
+//        
+//        $codeblock = '<code>' . $code . '</code>';
+//        return $this->hashBlock($codeblock);
+//    }
 }
