d6a251ef9d1cd387bb7ebdd207ed6774987f395a
Thomas Tourlourat <thomas@tourlourat.com>
2013-12-27 15:25:29 +0100

    Add production stage for capistrano

--- capistrano/stages/production.rb
+++ capistrano/stages/production.rb
@@ -1,9 +1,7 @@
 set :stage, :production
 
-set :ssh_user, 'nil'
+set :ssh_user, 'thomas'
 set :branch, 'master'
 set :deploy_to, '/var/www/vhosts/com.wozbe.www'
-
-SSHKit.config.command_map[:composer] = "#{shared_path.join("composer.phar")}"
 
 server 'arthos.armetiz.info', user: fetch(:ssh_user), roles: %w{web app db}
