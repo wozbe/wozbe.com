339598d2e9d7c20ebc10c3c801da7bc5dd85eb10
Thomas Tourlourat <thomas@tourlourat.com>
2014-01-21 17:20:46 +0100

    Remove useless services.xml

--- src/Wozbe/PageBundle/DependencyInjection/WozbePageExtension.php
+++ src/Wozbe/PageBundle/DependencyInjection/WozbePageExtension.php
@@ -23,6 +23,5 @@ class WozbePageExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
     }
 }
